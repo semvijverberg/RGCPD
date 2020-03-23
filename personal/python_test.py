f60f05882d0e37215779632c8912cfeaef00dc9a
Sem Vijverberg <s.p.vijverberg@vu.nl>
2020-03-23 12:17:46 +0100

    update for cluster

--- personal/python_test.py
+++ personal/python_test.py
@@ -3,20 +3,22 @@ from concurrent.futures import ProcessPoolExecutor
 import multiprocessing
 from time import sleep, time
 max_cpu = multiprocessing.cpu_count()
-print(f'{max_cpu} cpu\'s detected')
+use_cpu = min(16, max_cpu)
+print(f'{max_cpu} cpu\'s available, using {use_cpu}')
 
 
-def poolworker(index):
-    sleep(20)
-    print(index)
-    return index
+def poolworker(job_executed):
+    print(job_executed)
+    sleep(10)
+    return job_executed
 
 n_jobs = 16
+
+
 t0 = time()
-pool = ProcessPoolExecutor(max_cpu)
+pool = ProcessPoolExecutor(max_workers=use_cpu)
 futures = {} 
 for i in range(n_jobs):
-    futures[i] = pool.submit(poolworker, i)
+    futures[i] = pool.submit(poolworker, i+1)
 results = {key:future.result() for key, future in futures.items()}
-
 print(f'Time elapsed {int(time() - t0)}')
