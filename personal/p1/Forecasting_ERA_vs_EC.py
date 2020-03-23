f4919a4b4f791809ae30fc68a04bb7786c0bc697
Sem Vijverberg <s.p.vijverberg@vu.nl>
2020-03-23 16:25:50 +0100

    update for cluster

---
+++ personal/p1/Forecasting_ERA_vs_EC.py
@@ -0,0 +1,258 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Forecasting
+# Below done with test data, same format as df_data
+
+#get_ipython().run_line_magic('load_ext', 'autoreload')
+#get_ipython().run_line_magic('autoreload', '2')
+import os, inspect, sys
+import numpy as np
+curr_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))) # script directory
+main_dir = '/'.join(curr_dir.split('/')[:-1])
+RGCPD_dir = os.path.join(main_dir, 'RGCPD')
+fc_dir = os.path.join(main_dir, 'forecasting')
+df_ana_dir = os.path.join(main_dir, 'df_analysis/df_analysis/')
+if fc_dir not in sys.path:
+    sys.path.append(main_dir)
+    sys.path.append(RGCPD_dir)
+    sys.path.append(df_ana_dir)
+    sys.path.append(fc_dir)
+
+user_dir = os.path.expanduser('~')
+if sys.platform == 'linux':
+    import matplotlib as mpl
+    mpl.use('Agg')
+
+
+
+from class_fc import fcev
+
+
+# In[4]:
+
+
+
+
+
+# Define statmodel:
+logit = ('logit', None)
+
+logitCV = ('logitCV',
+          {'Cs':np.logspace(-4,1,10),
+          'class_weight':{ 0:1, 1:1},
+           'scoring':'brier_score_loss',
+           'penalty':'l2',
+           'solver':'lbfgs',
+           'max_iter':150})
+
+
+logitCVfs = ('logitCV',
+          {'class_weight':{ 0:1, 1:1},
+           'scoring':'brier_score_loss',
+           'penalty':'l2',
+           'solver':'lbfgs',
+           'feat_sel':{'model':None}})
+
+GBC_tfs = ('GBC',
+          {'max_depth':[1, 2, 3, 4],
+           'learning_rate':[1E-2, 5E-3, 1E-3, 5E-4],
+           'n_estimators' : [200, 300, 400, 500, 600, 700, 800, 1000],
+           'min_samples_split':[.15, .25],
+           'max_features':[.2,'sqrt', .5],
+           'subsample' : [.3, .4, .5, 0.6],
+           'random_state':60,
+           'scoringCV':'brier_score_loss',
+           'feat_sel':{'model':None} } )
+
+GBC_t = ('GBC', 
+         {'max_depth':[1, 2, 3, 4, 5, 6],
+           'learning_rate':5E-4,
+           'n_estimators' : [500, 750, 1000, 1250, 1500],
+           'min_samples_split':.2,
+           'max_features':[.15, .2, 'sqrt'],
+           'subsample' : [.3, .45],
+           'random_state':60,
+           # 'n_iter_no_change':20,
+           # 'tol':1E-4,
+           # 'validation_fraction':.3,
+           'scoringCV':'brier_score_loss' } )
+
+
+GBC = ('GBC',
+      {'max_depth':1,
+       'learning_rate':.05,
+       'n_estimators' : 500,
+       'min_samples_split':.25,
+       'max_features':.4,
+       'subsample' : .45,
+       'random_state':60,
+       'n_iter_no_change':20,
+       'tol':1E-4,
+       'validation_fraction':.3,
+       'scoringCV':'brier_score_loss'
+       } )
+
+# In[6]:
+EC_data  = user_dir + '/surfdrive/output_RGCPD/easternUS_EC/EC_tas_tos_Northern/958dd_ran_strat10_s30/data/EC_21-03-20_16hr_lag_0_958dd.h5'
+ERA_data = user_dir + '/surfdrive/output_RGCPD/easternUS/ERA5_mx2t_sst_Northern/ff393_ran_strat10_s30/data/ERA5_21-03-20_12hr_lag_0_ff393.h5'
+
+# path_ts = '/Users/semvijverberg/surfdrive/MckinRepl/RVts'
+# RVts_filename = '/Users/semvijverberg/surfdrive/MckinRepl/RVts/era5_t2mmax_US_1979-2018_averAggljacc0.25d_tf1_n4__to_t2mmax_US_tf1_selclus4_okt19_Xzkup1.npy'
+# filename_ts = os.path.join(path_ts, RVts_filename)
+# kwrgs_events_daily =    (filename_ts,
+#                          {'event_percentile': 90})
+
+kwrgs_events = {'event_percentile': 'std'}
+
+kwrgs_events = kwrgs_events
+precur_aggr = 1
+use_fold = None
+lags_i = np.array([0, 14, 21, 28, 35])
+start_end_TVdate = None # ('7-04', '8-22')
+
+
+list_of_fc = [fcev(path_data=ERA_data, precur_aggr=precur_aggr, 
+                    use_fold=use_fold, start_end_TVdate=None,
+                    stat_model=logitCV, 
+                    kwrgs_pp={'add_autocorr':False, 'normalize':'datesRV'}, 
+                    dataset=f'ERA-5',
+                    keys_d='PEP'),
+              fcev(path_data=ERA_data, precur_aggr=precur_aggr, 
+                    use_fold=use_fold, start_end_TVdate=None,
+                    stat_model=logitCV, 
+                    kwrgs_pp={'add_autocorr':False, 'normalize':'datesRV'}, 
+                    dataset=f'ERA-5',
+                    keys_d='CPPA'),
+              fcev(path_data=EC_data, precur_aggr=precur_aggr, 
+                    use_fold=use_fold, start_end_TVdate=None,
+                    stat_model=logitCV, 
+                    kwrgs_pp={'add_autocorr':False, 'normalize':'datesRV'}, 
+                    dataset=f'EC-earth',
+                    keys_d='PEP'),
+              fcev(path_data=EC_data, precur_aggr=precur_aggr, 
+                    use_fold=use_fold, start_end_TVdate=None,
+                    stat_model=logitCV, 
+                    kwrgs_pp={'add_autocorr':False, 'normalize':'datesRV'}, 
+                    dataset=f'EC-earth',
+                    keys_d='CPPA')]
+
+              
+fc = list_of_fc[0]
+#%%
+for i, fc in enumerate(list_of_fc):
+    
+    fc.get_TV(kwrgs_events=kwrgs_events)
+    
+    fc.fit_models(lead_max=lags_i, verbosity=1)
+
+    fc.perform_validation(n_boot=500, blocksize='auto', alpha=0.05,
+                          threshold_pred=(1.5, 'times_clim'))
+    
+
+# In[8]:
+working_folder, filename = fc._print_sett(list_of_fc=list_of_fc)
+
+store = False
+if __name__ == "__main__":
+    filename = fc.filename 
+    store = True
+
+import valid_plots as dfplots
+import functions_pp
+kwrgs = {'wspace':0.25, 'col_wrap':None}
+#kwrgs = {'wspace':0.25, 'col_wrap':3, 'threshold_bin':fc.threshold_pred}
+met = ['AUC-ROC', 'AUC-PR', 'BSS', 'Rel. Curve']
+#met = ['AUC-ROC', 'AUC-PR', 'BSS', 'Rel. Curve']
+
+
+line_dim = 'exper'
+
+
+dict_all = dfplots.merge_valid_info(list_of_fc, store=store)
+if store:
+    dict_all = functions_pp.load_hdf5(filename+'.h5')
+
+fig = dfplots.valid_figures(dict_all, 
+                          line_dim=line_dim,
+                          group_line_by=None,
+                          met=met, **kwrgs)
+
+f_format = '.pdf'
+pathfig_valid = os.path.join(filename + f_format)
+fig.savefig(pathfig_valid,
+            bbox_inches='tight') # dpi auto 600
+
+
+
+#%%
+
+im = 0
+il = 1
+ifc = 0
+f_format = '.pdf'
+if os.path.isdir(fc.filename) == False : os.makedirs(fc.filename)
+import valid_plots as dfplots
+if __name__ == "__main__":
+    for ifc, fc in enumerate(list_of_fc):
+        for im, m in enumerate([n[0] for n in fc.stat_model_l]):
+            for il, l in enumerate(fc.lags_i):
+                fc = list_of_fc[ifc]
+                m = [n[0] for n in fc.stat_model_l][im]
+                l = fc.lags_i[il]
+                # visual analysis
+                f_name = os.path.join(filename, f'ifc{ifc}_va_l{l}_{m}')
+                fig = dfplots.visual_analysis(fc, lag=l, model=m)
+                fig.savefig(os.path.join(working_folder, f_name) + f_format, 
+                            bbox_inches='tight') # dpi auto 600
+                # plot deviance
+                if m[:3] == 'GBC':
+                    fig = dfplots.plot_deviance(fc, lag=l, model=m)
+                    f_name = os.path.join(filename, f'ifc{ifc}_deviance_l{l}')
+                    
+
+                    fig.savefig(os.path.join(working_folder, f_name) + f_format,
+                                bbox_inches='tight') # dpi auto 600
+                    
+                    fig = fc.plot_oneway_partial_dependence()
+                    f_name = os.path.join(filename, f'ifc{ifc}_partial_depen_l{l}')
+                    fig.savefig(os.path.join(working_folder, f_name) + f_format,
+                                bbox_inches='tight') # dpi auto 600
+                    
+                if m[:7] == 'logitCV':
+                    fig = fc.plot_logit_regularization(lag_i=l)
+                    f_name = os.path.join(filename, f'ifc{ifc}_logitregul_l{l}')
+                    fig.savefig(os.path.join(working_folder, f_name) + f_format,
+                            bbox_inches='tight') # dpi auto 600
+                
+            df_importance, fig = fc.plot_feature_importances()
+            f_name = os.path.join(filename, f'ifc{ifc}_feat_l{l}_{m}')
+            fig.savefig(os.path.join(working_folder, f_name) + f_format, 
+                        bbox_inches='tight') # dpi auto 600
+
+
+
+#rename_ERA =    {'ERA-5: sst(PEP)+sm':'PEP+sm', 
+#             'ERA-5: sst(PDO,ENSO)+sm':'PDO+ENSO+sm', 
+#             'ERA-5: sst(CPPA)+sm':'CPPA+sm'}
+#
+#for old, new in rename_ERA.items():
+#    if new not in dict_experiments.keys():
+#        dict_experiments[new] = dict_experiments.pop(old)
+
+#rename_EC = {'ERA-5 PEP':'PEP', 
+#             'ERA-5 CPPA':'CPPA', 
+#             'EC-earth 2.3 PEP':'PEP ', 
+#             'EC-earth 2.3 CPPA':'CPPA '}
+#
+#for old, new in rename_EC.items():
+#    if new not in dict_experiments.keys():
+#        dict_experiments[new] = dict_experiments.pop(old)
+
+#rename_CPPA_comp =    {'ERA-5: CPPAregs+sm' : 'precursor regions + sm', 
+#                       'ERA-5: CPPApattern+sm': 'precursor pattern + sm', 
+#                       'ERA-5: sst(CPPA)+sm' : 'CPPA (all) + sm'}
+#
+#for old, new in rename_CPPA_comp.items():
+#    if new not in dict_experiments.keys():
+#        dict_experiments[new] = dict_experiments.pop(old)
---
+++ personal/p1/Teleconnections_E-US.py
@@ -0,0 +1,202 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# get_ipython().run_line_magic('load_ext', 'autoreload')
+# get_ipython().run_line_magic('autoreload', '2')
+
+import os, inspect, sys
+if sys.platform == 'linux':
+    import matplotlib as mpl
+    mpl.use('Agg')
+user_dir = os.path.expanduser('~')
+
+curr_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))) # script directory
+main_dir = '/'.join(curr_dir.split('/')[:-1])
+RGCPD_func = os.path.join(main_dir, 'RGCPD')
+cluster_func = os.path.join(main_dir, 'clustering/') 
+if cluster_func not in sys.path:
+    sys.path.append(main_dir)
+    sys.path.append(RGCPD_func)
+    sys.path.append(cluster_func)
+
+import numpy as np
+# ## Initialize RGCPD class
+# args:
+# - list_of_name_path
+# - start_end_TVdate
+# 
+#         list_of_name_path : list of name, path tuples. 
+#         Convention: first entry should be (name, path) of target variable (TV).
+#         list_of_name_path = [('TVname', 'TVpath'), ('prec_name1', 'prec_path1')]
+#         
+#         TV period : tuple of start- and enddate in format ('mm-dd', 'mm-dd')
+
+# In[5]:
+
+
+from RGCPD import RGCPD
+from RGCPD import EOF
+from RGCPD import BivariateMI
+
+
+CPPA_s30 = [('sst_CPPAs30', '/Users/semvijverberg/surfdrive/MckinRepl/era5_T2mmax_sst_Northern/Xzkup1_ran_strat10_s30/data/era5_21-01-20_10hr_lag_10_Xzkup1.h5' )]
+CPPA_s5  = [('sst_CPPAs5', '/Users/semvijverberg/surfdrive/MckinRepl/era5_T2mmax_sst_Northern/Xzkup1_ran_strat10_s5/data/ERA5_15-02-20_15hr_lag_10_Xzkup1.h5')]
+
+#list_of_name_path = [('t2mmmax',
+#                      '/Users/semvijverberg/surfdrive/MckinRepl/RVts/era5_t2mmax_US_1979-2018_averAggljacc0.25d_tf1_n4__to_t2mmax_US_tf1_selclus4_okt19_Xzkup1.npy'),
+#                        ('sm1', '/Users/semvijverberg/surfdrive/ERA5/input_raw/sm_1_1979-2018_1_12_daily_1.0deg.nc'),
+#                        ('sm2', '/Users/semvijverberg/surfdrive/ERA5/input_raw/sm_2_1979-2018_1_12_daily_1.0deg.nc'),
+#                        ('sm3', '/Users/semvijverberg/surfdrive/ERA5/input_raw/sm_3_1979-2018_1_12_daily_1.0deg.nc'),
+#                        ('u500', '/Users/semvijverberg/surfdrive/ERA5/input_raw/u500hpa_1979-2018_1_12_daily_2.5deg.nc'),
+#                        ('sst', '/Users/semvijverberg/surfdrive/ERA5/input_raw/sst_1979-2018_1_12_daily_1.0deg.nc')]
+
+list_of_name_path = [('t2mmmax',
+                      '/Users/semvijverberg/surfdrive/MckinRepl/RVts/era5_t2mmax_US_1979-2018_averAggljacc0.25d_tf1_n4__to_t2mmax_US_tf1_selclus4_okt19_Xzkup1.npy'),
+                        # ('sm1', '/Users/semvijverberg/surfdrive/ERA5/input_raw/sm1_1979-2018_1_12_daily_1.0deg.nc'),
+                        ('sm2', '/Users/semvijverberg/surfdrive/ERA5/input_raw/sm2_1979-2018_1_12_daily_1.0deg.nc'),                    
+                        ('sm3', '/Users/semvijverberg/surfdrive/ERA5/input_raw/sm3_1979-2018_1_12_daily_1.0deg.nc'),  
+                        ('sst', '/Users/semvijverberg/surfdrive/ERA5/input_raw/sst_1979-2018_1_12_daily_1.0deg.nc'),                        
+                        # ('snow', '/Users/semvijverberg/surfdrive/ERA5/input_raw/snow_1979-2018_1_12_daily_1.0deg.nc'),
+                        ('OLR', '/Users/semvijverberg/surfdrive/ERA5/input_raw/OLRtrop_1979-2018_1_12_daily_2.5deg.nc')]
+
+#                        ('u500', '/Users/semvijverberg/surfdrive/ERA5/input_raw/u500hpa_1979-2018_1_12_daily_2.5deg.nc'),
+#                        ('v200', '/Users/semvijverberg/surfdrive/ERA5/input_raw/v200hpa_1979-2018_1_12_daily_2.5deg.nc'),                        
+
+#                        ('sm123', '/Users/semvijverberg/surfdrive/ERA5/input_raw/sm_123_1979-2018_1_12_daily_1.0deg.nc')]
+
+import_prec_ts = None #CPPA_s30
+
+list_for_EOFS = [EOF(name='OLR', neofs=1, selbox=[-180, 360, -15, 30])]
+
+list_for_MI   = [BivariateMI(name='sst', func=BivariateMI.corr_map, 
+                             kwrgs_func={'alpha':.05, 'FDR_control':True}, 
+                             distance_eps=600, min_area_in_degrees2=5),
+                  BivariateMI(name='sm3', func=BivariateMI.corr_map, 
+                              kwrgs_func={'alpha':.05, 'FDR_control':True}, 
+                              distance_eps=600, min_area_in_degrees2=7)]
+                 # BivariateMI(name='snow', func=BivariateMI.corr_map, 
+                 #             kwrgs_func={'alpha':.05, 'FDR_control':True}, 
+                 #             distance_eps=600, min_area_in_degrees2=5)]
+                            
+
+#list_of_name_path = [('t2mmmax',
+#                      '/Users/semvijverberg/surfdrive/MckinRepl/RVts/era5_t2mmax_US_1979-2018_averAggljacc0.25d_tf1_n4__to_t2mmax_US_tf1_selclus4_okt19.npy'),
+#                    ('sst', '/Users/semvijverberg/surfdrive/Data_era5/input_raw/sst_1979-2018_1_12_daily_1.0deg.nc'),
+#                    ('sm123', '/Users/semvijverberg/surfdrive/Data_era5/input_raw/sm_123_1979-2018_1_12_daily_0.25deg.nc'),
+#                    ('v200hpa', '/Users/semvijverberg/surfdrive/Data_era5/input_raw/v200hpa_1979-2018_1_12_daily_2.5deg.nc')]
+
+start_end_TVdate = ('06-24', '08-22')
+# start_end_TVdate = ('07-06', '08-11')
+
+#start_end_TVdate = ('06-15', '08-31')
+start_end_date = ('1-1', '12-31')
+
+
+rg = RGCPD(list_of_name_path=list_of_name_path, 
+           list_for_EOFS=list_for_EOFS,
+           list_for_MI=list_for_MI,
+           import_prec_ts=import_prec_ts,
+           start_end_TVdate=start_end_TVdate,
+           start_end_date=start_end_date,
+           tfreq=10, lags_i=np.array([1]),
+           path_outmain=user_dir+'/surfdrive/output_RGCPD')
+
+
+# In[6]:
+
+#selbox = [None, {'sst':[-180,360,-10,90]}]
+selbox = None
+#anomaly = [True, {'sm1':False, 'sm2':False, 'sm3':False}]
+anomaly = [True, {'sm1':False, 'sm2':False, 'sm3':False, 'st2':False}]
+
+rg.pp_precursors(selbox=selbox, anomaly=anomaly)
+
+
+# ### Post-processing Target Variable
+
+# In[7]:
+
+
+rg.pp_TV()
+
+
+
+# In[165]:
+
+#kwrgs_events={'event_percentile':66}
+kwrgs_events=None
+rg.traintest(method='random10', kwrgs_events=kwrgs_events)
+
+
+
+# In[166]:
+
+
+rg.calc_corr_maps() 
+
+
+# In[167]:
+
+
+rg.cluster_list_MI()
+
+
+# In[168]:
+
+
+rg.quick_view_labels() 
+
+
+# In[169]:
+
+
+rg.get_ts_prec(precur_aggr=None)
+
+
+# In[170]:
+
+
+rg.df_data
+
+# rg.store_df()
+
+# In[171]:
+
+# rg.get_ts_prec(precur_aggr=None)
+rg.PCMCI_df_data(pc_alpha=None, 
+                 tau_max=2,
+                 alpha_level=0.1, 
+                 max_combinations=2)
+rg.df_sum
+
+# In[172]:
+
+
+rg.plot_maps_sum()
+
+
+# In[173]:
+
+
+#rg.df_data
+
+
+
+
+# In[ ]:
+rg.get_ts_prec(precur_aggr=1)
+rg.store_df_PCMCI()
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
