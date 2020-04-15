fixing getting causal links and finding zvar

RGCPD/class_EOF.py
@@ -151,7 +151,7 @@ class EOF:
     def _single_split(func, ds_EOF, s, df_splits, neofs):
         splits = df_splits.index.levels[0]
         progress = 100 * (s+1) / splits.size
-        print(f"\rProgress traintest set {progress}%)", end="")
+        print(f"\rProgress traintest set {progress}%", end="")
         dates_train = functions_pp.dfsplits_to_dates(df_splits, s)[0]
         # convert Train test year from original time to monthly
         train_yrs = np.unique(dates_train.year)

RGCPD/class_RGCPD.py
@@ -404,16 +404,18 @@ class RGCPD:
         if hasattr(self, 'pcmci_results_dict')==False:
             print('first perform PCMCI_df_data to get pcmci_results_dict')
         
-        if alpha_level is None:
-            alpha_level = self.kwrgs_TV['alpha_level']
         self.parents_dict = wrapper_PCMCI.get_links_pcmci(self.pcmci_dict, 
                                                           self.pcmci_results_dict,
                                                           alpha_level)
-        self.df_links, self.mapping_links = wrapper_PCMCI.get_df_sum(self.parents_dict)
+        self.df_links = wrapper_PCMCI.get_df_links(self.parents_dict)
+        lags = np.arange(self.kwrgs_pcmci['tau_min'], self.kwrgs_pcmci['tau_max']+1)
+        self.df_MCI   = wrapper_PCMCI.get_df_MCI(self.pcmci_dict, 
+                                                 self.pcmci_results_dict, 
+                                                 lags, self.TV.name)
 
-        # get xarray dataset for each variable
-        self.dict_ds = plot_maps.causal_reg_to_xarray(self.TV.name, self.df_links,
-                                                      self.list_for_MI)
+        # # get xarray dataset for each variable
+        # self.dict_ds = plot_maps.causal_reg_to_xarray(self.TV.name, self.df_links,
+        #                                               self.list_for_MI)
 
     def store_df_PCMCI(self):
         import wrapper_PCMCI

RGCPD/wrapper_PCMCI.py
@@ -7,7 +7,7 @@ from tigramite.independence_tests import ParCorr #, GPDC, CMIknn, CMIsymb
 import numpy as np
 import pandas as pd
 import itertools
-
+flatten = lambda l: list(itertools.chain.from_iterable(l))
 
 
 
@@ -193,35 +193,35 @@ def get_links_pcmci(pcmci_dict, pcmci_results_dict, alpha_level):
         
         pcmci = pcmci_dict[s]
         results = pcmci_results_dict[s]
-        # returns all causal links, not just causal parents/precursors (of lag>0)
-        sig = return_sign_links(pcmci, pq_matrix=results['q_matrix'],
-                                            val_matrix=results['val_matrix'],
-                                            alpha_level=alpha_level)
+        # # returns all causal links, not just causal parents/precursors (of lag>0)
+        # sig = return_sign_links(pcmci, pq_matrix=results['q_matrix'],
+        #                                     val_matrix=results['val_matrix'],
+        #                                     alpha_level=alpha_level)
+        
+        sig = pcmci.return_significant_parents(results['q_matrix'], 
+                                               val_matrix=results['val_matrix'],
+                                               alpha_level=alpha_level)
 
         all_parents = sig['parents']
-    #    link_matrix = sig['link_matrix']
+        link_matrix = sig['link_matrix']
     
         links_RV = all_parents[0]
-        parents_dict[s] = links_RV, pcmci.var_names
+        parents_dict[s] = links_RV, pcmci.var_names, link_matrix
         
     #%%
     return parents_dict
 
-def get_df_sum(parents_dict):
+def get_df_links(parents_dict):
     splits = np.array(list(parents_dict.keys()))
-    df_sum_s = np.zeros( (splits.size) , dtype=object)
-    mapping_links_dict = {}
+    df_links_s = np.zeros( (splits.size) , dtype=object)
     for s in range(splits.size):
-        links_RV, var_names = parents_dict[s] 
-        df, mapping_links = bookkeeping_precursors(links_RV, var_names)
-        df_sum_s[s] = df
+        links_RV, var_names, link_matrix = parents_dict[s] 
+        df = pd.DataFrame(link_matrix[0], index=var_names)
+        df_links_s[s] = df
         
-        mapping_links_dict[s] = mapping_links
-    df_sum = pd.concat(list(df_sum_s), keys= range(splits.size))
-    count_causal = df_sum.loc[:,'causal'].sum(level=[1]).astype(int)
-    df_sum['count'] = pd.concat([count_causal] * splits.size, 
-                                   keys=range(splits.size))
-    return df_sum, mapping_links_dict
+    df_links = pd.concat(list(df_links_s), keys= range(splits.size))
+
+    return df_links
 
 def return_sign_links(pc_class, pq_matrix, val_matrix,
                             alpha_level=0.05):
@@ -240,158 +240,167 @@ def return_sign_links(pc_class, pq_matrix, val_matrix,
             'link_matrix': pq_matrix <= alpha_level}
 
 
-def bookkeeping_precursors(links_RV, var_names):
-    #%%
-    # if links_RV[0][0] == 0: # index 0 should refer to RV_name
-    RV_name = var_names[0]
-    # else:
-        # RV_name = None
-    var_names_ = var_names.copy()
-    mapping_links = [(var_names_[l[0]],l[1]) for l in links_RV]
-    lin_RVsor = sorted(links_RV)
-    index = [n.split('..')[1] for n in var_names_[1:]] ; 
-    index.insert(0, var_names_[0])
-    link_names = [var_names_[l[0]].split('..')[1] if l[0] !=0 else var_names_[l[0]] for l in lin_RVsor]
 
-    # check if two lags of same region and are tigr significant
-    idx_tigr = [l[0] for l in lin_RVsor] ;
-    var_names_ext = var_names_.copy()
-    index_ext = index.copy()
-    for r in np.unique(idx_tigr):
-        # counting double indices (but with different lags)
-        if idx_tigr.count(r) != 1:
-            if var_names_[r] == RV_name:
-                double = var_names_[r].split('..')[0]
-            else:
-                double = var_names_[r].split('..')[1]
-#            print(double)
-            idx = int(np.argwhere(np.array(index)==double)[0])
-            # append each double to index for the dataframe
-            for i in range(idx_tigr.count(r)-1):
-                index_ext.insert(idx+i, double)
-                d = len(index) - len(var_names_)
-#                var_names_ext.insert(idx+i+1, var_names_[idx+1-d])
-                var_names_ext.insert(idx+i, var_names_[idx-d])            
-
-    # retrieving only var name
-    l = [n.split('..')[-1] for n in var_names_ext[1:]]
-    l.insert(0, var_names_ext[0])
-    var = np.array(l)
-    # creating mask (True) of causal links
-    mask_causal = np.array([True if i in link_names else False for i in index_ext])
-    # retrieving lag of corr map
+def get_df_MCI(pcmci_dict, pcmci_results_dict, lags, variable):
+    splits = np.array(list(pcmci_dict.keys()))
+    df_MCIc_s = np.zeros( (splits.size) , dtype=object)
+    df_MCIa_a = np.zeros( (splits.size) , dtype=object)
+    for s in range(splits.size):
+        pcmci_class = pcmci_dict[s]
+        results_dict = pcmci_results_dict[s]
+        var_names = pcmci_class.var_names
+        idx = var_names.index(variable)
+        try:
+            pvals = results_dict['q_matrix'][idx]
+            c = 'qval'
+        except:
+            c = 'pval'
+            pvals = results_dict['p_matrix'][idx]
+        coeffs = results_dict['val_matrix'][idx]
+        # data = np.concatenate([coeffs, pvals],  1)
+        
+        cols = [f'coeff l{l}' for l in lags]
+        # cols.append([f'pval l{l}' for l in lags])
+        df_coeff = pd.DataFrame(coeffs, index=var_names, 
+                          columns=cols)
+        df_MCIc_s[s] = df_coeff
+        cols = [f'{c} l{l}' for l in lags]
+        df_alphas = pd.DataFrame(pvals, index=var_names, 
+                          columns=cols)
+        df_MCIa_a[s] = df_alphas
+    df_MCIc = pd.concat(list(df_MCIc_s), keys= range(splits.size))
+    df_MCIa = pd.concat(list(df_MCIa_a), keys= range(splits.size))
+    return df_MCIc, df_MCIa
     
-    TV_lag = np.repeat(0, var_names_ext.count(RV_name))
-    prec_corr_map = np.array([int(n.split('..')[0]) for n in var_names_ext if n != RV_name]) ;
-    lag_corr_map = np.insert(prec_corr_map, TV_lag, 0) # unofficial lag for TV
+    
 
-    # retrieving region number, corresponding to figures
-    region_number = np.array([int(n.split('..')[1]) for n in var_names_ext if n != RV_name])
-    # unofficial region  number for TV
-    region_number = np.insert(region_number, TV_lag, 0) 
-    # retrieving lag of tigramite link
-    # all Tigr links, can include same region at multiple lags:
-    # looping through all unique tigr var labels format {lag..var_name}
-    lag_tigr_map = {str(lin_RVsor[i][1])+'..'+link_names[i]:lin_RVsor[i][1] for i in range(len(link_names))}
-    sorted(lag_tigr_map, reverse=False)
-    # fill in the nans where the var_name is not causal
-    lag_tigr_ = index_ext.copy() ; track_idx = list(range(len(index_ext)))
-    # looping through all unique tigr var labels and tracking the concomitant indices 
-    for k in lag_tigr_map.keys():
-        l = int(k.split('..')[0])
-        var_temp = k.split('..')[1]
-        idx = lag_tigr_.index(var_temp)
-        track_idx.remove(idx)
-        lag_tigr_[idx] = l
-    for i in track_idx:
-        lag_tigr_[i] = np.nan
-    lag_tigr_ = np.array(lag_tigr_)
 
-    mask_causal = ~np.isnan(lag_tigr_)
-
-    # print(var.shape, lag_corr_map.shape, region_number.shape, mask_causal.shape, lag_tigr_.shape)
-
-    data = np.concatenate([lag_corr_map[None,:], region_number[None,:], var[None,:],
-                            mask_causal[None,:], lag_tigr_[None,:]], axis=0)
-    df = pd.DataFrame(data=data.T, index=var_names_ext,
-                      columns=['lag_corr', 'region_number', 'var', 'causal', 'lag_tig'])
-    df['causal'] = df['causal'] == 'True'
-    df = df.astype({'lag_corr':int,
-                               'region_number':int, 'var':str, 'causal':bool, 'lag_tig':float})
+def extract_ParCorr_info_from_text(filepath_txt=str, variable=str, pc_alpha='auto'):
     #%%
-    return df, mapping_links
+    start_variable_line = f'## Variable {variable}\n'
+    get_pc_alpha_lines = f'# pc_alpha = {pc_alpha}'
+    convergence_line = 'converged'
+    
+    if pc_alpha == 'auto':
+        pc_alpha = print_pc_alphas_summ_from_txt(filepath_txt, variable)
+    
+    # get max_conds_dim parameter
+    with open (filepath_txt, 'rt') as myfile:
+        for i, myline in enumerate(myfile):           
+            if 'max_conds_dim = ' in myline:
+                max_conds_dim = int(myline.split(' = ')[1])
+                break
+        
+    lines = [] ; 
+    start_var = False ; start_pc_alpha = False    
+    
+    var_kickedout = 'Non-significance detected.'
+    with open (filepath_txt, 'rt') as myfile:
+        for i, myline in enumerate(myfile):      
+            if start_variable_line == myline :
+                lines.append(myline)
+                start_var = True
+            if start_var and get_pc_alpha_lines in myline:
+                start_pc_alpha = True
+            if start_pc_alpha:
+                lines.append(myline)
+            if start_var and start_pc_alpha and convergence_line in myline:
+                break
+            
+    # collect init OLR results
+    tested_links = [] ; pvalues = [] ; coeffs = [] 
+    track = False
+    start_init = 'Testing condition sets of dimension 0:'
+    end_init   = 'Updating parents:'
+    init_OLR = 'No conditions of dimension 0 left.' 
+    for i, myline in enumerate(lines):
+        
+        if start_init in myline:
+            track = True
+        if track:
+            if 'Link' in myline:
+                # print(subline)
+                link = myline
+                var = link.split('Link (')[1].split(')')[0]
+                tested_links.append(var)
+            if 'pval' in myline:
+                OLR = myline
+                p = float(OLR.split('pval = ')[1].split(' / ')[0])
+                pvalues.append(p)
+                c = float(OLR.split(' val = ')[1].replace('\n',''))
+                coeffs.append(c)
+        if end_init in myline:
+            break
+    
+    OLR_data = np.concatenate([np.array(coeffs)[:,None], np.array(pvalues)[:,None]], 
+                              axis=1)
+    df_OLR = pd.DataFrame(data=OLR_data, index=tested_links, 
+                          columns=['coeff', 'pval'])
+    
+    # Find by which (set of) var(s) the link was found C.I.
+    tested_links = [] ; pvalues = [] ; coeffs = [] ; by = {}
+    for i, myline in enumerate(lines):
+        
+        if init_OLR in myline:   
+            link = lines[i-2] 
+            var = link.split('Link (')[1].split(')')[0]
+            tested_links.append(var)
+            OLR = lines[i-1]
+            p = float(OLR.split('pval = ')[1].split(' / ')[0])
+            pvalues.append(p)
+            c = float(OLR.split(' val = ')[1].replace('\n',''))
+            coeffs.append(c)
+            
+        if var_kickedout in myline:
+            # Last line of xy == Non-significance detected.
+            # second last line is the test where precursor it was found C.I.
+            xy = lines[i-max_conds_dim-1 : i+1]            
+            # Search for first name tested link in lines above 'Non-significance detected.'
+            for subline in xy[::-1]:
+                if 'Link' in subline:
+                    link = subline
+                    var = link.split('Link (')[1].split(')')[0]
+                    tested_links.append(var)
+                    break
+            OLR = xy[-2]
+            p = float(OLR.split('pval = ')[1].split(' / ')[0])
+            pvalues.append(p)
+            c = float(OLR.split(' val = ')[1].replace('\n',''))
+            coeffs.append(c)
+                
+            if '(' in OLR:
+                zvar = OLR.split(': ')[1].split('  -->')[0]
+                by[var] = zvar
+            else:
+                by[var] = '-'
+                
+    for k in df_OLR.index:
+        # print(k)
+        if k not in by.keys():
+            by[k] = 'C.D.'
+    df_OLR['ParrCorr'] = df_OLR.index.map(by)
+    #%%
+    return df_OLR
 
-
-## =============================================================================
-## Haversine function
-## =============================================================================
-#from math import radians, cos, sin, asin, sqrt
-#from enum import Enum
-#
-#
-## mean earth radius - https://en.wikipedia.org/wiki/Earth_radius#Mean_radius
-#_AVG_EARTH_RADIUS_KM = 6371.0088
-#
-#
-#class Unit(Enum):
-#    """
-#    Enumeration of supported units.
-#    The full list can be checked by iterating over the class; e.g.
-#    the expression `tuple(Unit)`.
-#    """
-#
-#    KILOMETERS = 'km'
-#    METERS = 'm'
-#    MILES = 'mi'
-#    NAUTICAL_MILES = 'nmi'
-#    FEET = 'ft'
-#    INCHES = 'in'
-#
-#
-## Unit values taken from http://www.unitconversion.org/unit_converter/length.html
-#_CONVERSIONS = {Unit.KILOMETERS:       1.0,
-#                Unit.METERS:           1000.0,
-#                Unit.MILES:            0.621371192,
-#                Unit.NAUTICAL_MILES:   0.539956803,
-#                Unit.FEET:             3280.839895013,
-#                Unit.INCHES:           39370.078740158}
-#
-#
-#def haversine(point1, point2, unit=Unit.KILOMETERS):
-#    """ Calculate the great-circle distance between two points on the Earth surface.
-#    Takes two 2-tuples, containing the latitude and longitude of each point in decimal degrees,
-#    and, optionally, a unit of length.
-#    :param point1: first point; tuple of (latitude, longitude) in decimal degrees
-#    :param point2: second point; tuple of (latitude, longitude) in decimal degrees
-#    :param unit: a member of haversine.Unit, or, equivalently, a string containing the
-#                 initials of its corresponding unit of measurement (i.e. miles = mi)
-#                 default 'km' (kilometers).
-#    Example: ``haversine((45.7597, 4.8422), (48.8567, 2.3508), unit=Unit.METERS)``
-#    Precondition: ``unit`` is a supported unit (supported units are listed in the `Unit` enum)
-#    :return: the distance between the two points in the requested unit, as a float.
-#    The default returned unit is kilometers. The default unit can be changed by
-#    setting the unit parameter to a member of ``haversine.Unit``
-#    (e.g. ``haversine.Unit.INCHES``), or, equivalently, to a string containing the
-#    corresponding abbreviation (e.g. 'in'). All available units can be found in the ``Unit`` enum.
-#    """
-#
-#    # get earth radius in required units
-#    unit = Unit(unit)
-#    avg_earth_radius = _AVG_EARTH_RADIUS_KM * _CONVERSIONS[unit]
-#
-#    # unpack latitude/longitude
-#    lat1, lng1 = point1
-#    lat2, lng2 = point2
-#
-#    # convert all latitudes/longitudes from decimal degrees to radians
-#    lat1, lng1, lat2, lng2 = map(radians, (lat1, lng1, lat2, lng2))
-#
-#    # calculate haversine
-#    lat = lat2 - lat1
-#    lng = lng2 - lng1
-#    d = sin(lat * 0.5) ** 2 + cos(lat1) * cos(lat2) * sin(lng * 0.5) ** 2
-#
-#    return 2 * avg_earth_radius * asin(sqrt(d))
+def print_pc_alphas_summ_from_txt(filepath_txt=str, variable=str):
+    # get pc_alpha parameter from text
+    #%%
+    start_pc_alpha_sum = '# Condition selection results:'
+    var_searched = f'Algorithm converged for variable {variable}'
+    end_pc_alpha_sum   = f'--> optimal pc_alpha for variable {variable} is '
+    detected = False ; reached_pc_alpha_sum=False
+    with open (filepath_txt, 'rt') as myfile:
+        for i, myline in enumerate(myfile):           
+            if var_searched in myline:
+                detected = True
+            if detected and start_pc_alpha_sum in myline:
+                reached_pc_alpha_sum = True
+            if reached_pc_alpha_sum:
+                print(myline)
+                pc_alpha = float(myline.split(end_pc_alpha_sum)[1].split('\n')[0])
+                if end_pc_alpha_sum in myline:
+                    break
+    return pc_alpha
 
 
 # def print_particular_region_new(links_RV, var_names, s, outdic_precur, map_proj, ex):

personal/Circulation_US_heatwaves.py
@@ -32,11 +32,11 @@ from RGCPD import EOF
 
 
 TVpath = '/Users/semvijverberg/surfdrive/output_RGCPD/circulation_US_HW/tf5_nc5_dendo_80d77.nc'
-cluster_label = 3
-name_ds='ts'
-start_end_TVdate = ('06-24', '08-22')
+cluster_label = 5
+name_ds='q75tail'
+start_end_TVdate = ('06-01', '08-31')
 start_end_date = ('1-1', '12-31')
-tfreq = 14
+tfreq = 15
 #%%
 list_of_name_path = [(cluster_label, TVpath), 
                       ('v200', os.path.join(path_raw, 'v200hpa_1979-2018_1_12_daily_2.5deg.nc')),
@@ -82,30 +82,26 @@ rg.plot_maps_corr(aspect=4.5, cbar_vert=-.1, save=True)
 list_of_name_path = [(cluster_label, TVpath), 
                      ('z500',os.path.join(path_raw, 'z500hpa_1979-2018_1_12_daily_2.5deg.nc')),
                      ('sst', os.path.join(path_raw, 'sst_1979-2018_1_12_daily_1.0deg.nc')),
-                     ('sm2', os.path.join(path_raw, 'sm2_1979-2018_1_12_daily_1.0deg.nc')),
-                     ('sm3', os.path.join(path_raw, 'sm3_1979-2018_1_12_daily_1.0deg.nc')),
+                     ('sm123', os.path.join(path_raw, 'sm_123_1979-2018_1_12_daily_1.0deg.nc')),
                      ('snow',os.path.join(path_raw, 'snow_1979-2018_1_12_daily_1.0deg.nc')),
                      ('st2',  os.path.join(path_raw, 'lsm_st2_1979-2018_1_12_daily_1.0deg.nc')),
                      ('OLRtrop',  os.path.join(path_raw, 'OLRtrop_1979-2018_1_12_daily_2.5deg.nc'))]
 
 list_for_MI   = [BivariateMI(name='z500', func=BivariateMI.corr_map, 
                              kwrgs_func={'alpha':.01, 'FDR_control':True}, 
-                             distance_eps=600, min_area_in_degrees2=7, 
+                             distance_eps=700, min_area_in_degrees2=7, 
                              calc_ts='pattern cov'),
-                  BivariateMI(name='sst', func=BivariateMI.corr_map, 
+                 BivariateMI(name='sst', func=BivariateMI.corr_map, 
                               kwrgs_func={'alpha':.0001, 'FDR_control':True}, 
-                              distance_eps=600, min_area_in_degrees2=5),
-                  BivariateMI(name='sm2', func=BivariateMI.corr_map, 
-                               kwrgs_func={'alpha':.05, 'FDR_control':True}, 
-                               distance_eps=600, min_area_in_degrees2=5),
-                  BivariateMI(name='sm3', func=BivariateMI.corr_map, 
-                               kwrgs_func={'alpha':.05, 'FDR_control':True}, 
-                               distance_eps=700, min_area_in_degrees2=7),
-                  BivariateMI(name='snow', func=BivariateMI.corr_map, 
-                               kwrgs_func={'alpha':.05, 'FDR_control':True}, 
+                              distance_eps=700, min_area_in_degrees2=5),
+                 BivariateMI(name='sm123', func=BivariateMI.corr_map, 
+                               kwrgs_func={'alpha':.01, 'FDR_control':True}, 
+                               distance_eps=700, min_area_in_degrees2=5),
+                 BivariateMI(name='snow', func=BivariateMI.corr_map, 
+                               kwrgs_func={'alpha':.01, 'FDR_control':True}, 
                                distance_eps=700, min_area_in_degrees2=7),
                  BivariateMI(name='st2', func=BivariateMI.corr_map, 
-                               kwrgs_func={'alpha':.05, 'FDR_control':True}, 
+                               kwrgs_func={'alpha':.01, 'FDR_control':True}, 
                                distance_eps=700, min_area_in_degrees2=5)]
 
 list_for_EOFS = [EOF(name='OLRtrop', neofs=2, selbox=[-180, 360, -15, 30])]
@@ -117,12 +113,12 @@ rg = RGCPD(list_of_name_path=list_of_name_path,
            list_for_EOFS=list_for_EOFS,
            start_end_TVdate=start_end_TVdate,
            start_end_date=start_end_date,
-           tfreq=tfreq, lags_i=np.array([1]),
+           tfreq=tfreq, lags_i=np.array([0]),
            path_outmain=user_dir+'/surfdrive/output_RGCPD/circulation_US_HW',
            append_pathsub='_' + name_ds)
 
 rg.pp_TV(name_ds=name_ds)
-selbox = [None, {'sst':[-180,360,-10,90], 'z500':[130,350,10,90], 'v200':[130,350,10,90]}]
+selbox = [None, {'z500':[130,350,10,90], 'v200':[130,350,10,90]}]
 rg.pp_precursors(selbox=selbox)
 
 rg.traintest(method='random10')
@@ -139,8 +135,8 @@ rg.get_EOFs()
 
 rg.get_ts_prec(precur_aggr=None)
 rg.PCMCI_df_data(pc_alpha=None, 
-                 tau_max=1,
-                 max_conds_dim=2,
+                 tau_max=2,
+                 max_conds_dim=10,
                  max_combinations=3)
 rg.PCMCI_get_links(alpha_level=.01)
 rg.df_links.loc[4]
@@ -166,124 +162,17 @@ rg.store_df_PCMCI()
 
 
 #%%
+import pandas as pd
+flatten = lambda l: list(set([item for sublist in l for item in sublist]))
 
 variable = '5'
-s = 0
+s = 1
 pc_alpha = .05
-
-tig = rg.pcmci_dict[s]
-var_names = tig.var_names
-idx = var_names.index(variable)
-try:
-    pvals = rg.pcmci_results_dict[0]['q_matrix'][idx]
-except:
-    pvals = rg.pcmci_results_dict[0]['p_matrix'][idx]
-coeffs = rg.pcmci_results_dict[0]['val_matrix'][idx]
-data = np.concatenate([coeffs, pvals],  1)
-
-
-df_MCI = pd.DataFrame(data, index=var_names, 
-                  columns=['coeff_l0', 'coeff_l1', 'pval_l0', 'pval_l1'] )
-
-max_cond_dims = rg.kwrgs_pcmci['max_conds_dim']
+lags = range(0, rg.kwrgs_pcmci['tau_max']+1)
+pcmci_class = rg.pcmci_dict[s]
 
 filepath_txt = os.path.join(rg.path_outsub2, f'split_{s}_PCMCI_out.txt')
-#%%
-lines = [] ; 
-converged = False ; start_var = False ; start_pc_alpha = False
-start_variable_line = f'## Variable {variable}\n'
-get_pc_alpha_lines = f'# pc_alpha = {pc_alpha}'
-convergence_line = 'converged'
 
-var_kickedout = 'Non-significance detected.'
-with open (filepath_txt, 'rt') as myfile:
-    for i, myline in enumerate(myfile):      
-        if start_variable_line == myline :
-            lines.append(myline)
-            start_var = True
-        if start_var and get_pc_alpha_lines in myline:
-            start_pc_alpha = True
-        if start_pc_alpha:
-            lines.append(myline)
-        if start_var and start_pc_alpha and convergence_line in myline:
-            break
-        
-# collect init OLR
-tested_links = [] ; pvalues = [] ; coeffs = [] 
-track = False
-start_init = 'Testing condition sets of dimension 0:'
-end_init   = 'Updating parents:'
-init_OLR = 'No conditions of dimension 0 left.' 
-for i, myline in enumerate(lines):
-    
-    if start_init in myline:
-        track = True
-    if track:
-        if 'Link' in myline:
-            # print(subline)
-            link = myline
-            var = link.split('Link (')[1].split(')')[0]
-            tested_links.append(var)
-        if 'pval' in myline:
-            OLR = myline
-            p = float(OLR.split('pval = ')[1].split(' / ')[0])
-            pvalues.append(p)
-            c = float(OLR.split(' val = ')[1].replace('\n',''))
-            coeffs.append(c)
-    if end_init in myline:
-        break
-
-OLR_data = np.concatenate([np.array(coeffs)[:,None], np.array(pvalues)[:,None]], 
-                          axis=1)
-df_OLR = pd.DataFrame(data=OLR_data, index=tested_links, 
-                      columns=['coeff', 'pval'])
-#%%
-
-tested_links = [] ; pvalues = [] ; coeffs = [] ; by = {}
-for i, myline in enumerate(lines):
-    # print(myline)
-    # get max_cond_dims
-    if 'Testing condition sets of dimension' in myline:
-        max_cond_dims_i = int(myline.split(' ')[-1].split(':')[0])
-        max_cond_dims_i = max(1,min(max_cond_dims_i , max_cond_dims))
-    
-    if init_OLR in myline:   
-        # print(lines[i-2])
-        link = lines[i-2] 
-        var = link.split('Link (')[1].split(')')[0]
-        tested_links.append(var)
-        OLR = lines[i-1]
-        p = float(OLR.split('pval = ')[1].split(' / ')[0])
-        pvalues.append(p)
-        c = float(OLR.split(' val = ')[1].replace('\n',''))
-        coeffs.append(c)
-    if var_kickedout in myline:
-        # print(lines[i-1])
-        xy = lines[i-max_cond_dims_i-1 : i+1]
-        # print(xy)
-        for subline in xy:
-            if 'Link' in subline:
-                # print(subline)
-                link = subline
-                var = link.split('Link (')[1].split(')')[0]
-                tested_links.append(var)
-            if 'pval' in subline:
-                OLR = subline
-                p = float(OLR.split('pval = ')[1].split(' / ')[0])
-                pvalues.append(p)
-                c = float(OLR.split(' val = ')[1].replace('\n',''))
-                coeffs.append(c)
-        z  = lines[i-1]
-        if '(' in z:
-            zvar = z.split(': ')[1].split('  -->')[0]
-            by[var] = zvar
-        else:
-            by[var] = '-'
-for k in df_OLR.index:
-    print(k)
-    if k not in by.keys():
-        by[k] = 'C.D.'
-df_OLR['ParrCorr'] = df_OLR.index.map(by)
 
 
 #%%