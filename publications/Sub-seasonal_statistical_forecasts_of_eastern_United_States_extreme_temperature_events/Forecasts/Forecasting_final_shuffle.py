8530dddf70427e791c5fbd9e3374085ae78fe7bd
Sem Vijverberg <s.p.vijverberg@vu.nl>
2020-05-14 12:14:42 +0200

    update for cluster

--- publications/Sub-seasonal_statistical_forecasts_of_eastern_United_States_extreme_temperature_events/Forecasts/Forecasting_final_shuffle.py
+++ publications/Sub-seasonal_statistical_forecasts_of_eastern_United_States_extreme_temperature_events/Forecasts/Forecasting_final_shuffle.py
@@ -37,7 +37,7 @@ logit = ('logit', None)
 #%%
 start_time = time()
 
-ERA_data = data_dir + '/df_data_sst_CPPAs30_sm2_sm3_dt1_ff393.h5'
+ERA_data = data_dir + '/df_data_sst_CPPAs30_sm2_sm3_dt1_c378f.h5'
 
 kwrgs_events = {'event_percentile': 'std', 'window':'single_event', 'min_dur':3, 'max_break': 1}
 
