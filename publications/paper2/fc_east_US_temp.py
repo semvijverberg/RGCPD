7028a88bc3d914ab5b6b8d7638d5b19e88920c42
Sem Vijverberg <s.p.vijverberg@vu.nl>
2020-10-13 11:35:55 +0200

    update for cluster

--- publications/paper2/fc_east_US_temp.py
+++ publications/paper2/fc_east_US_temp.py
@@ -60,8 +60,8 @@ if target == 'easterntemp':
 start_end_date = ('1-1', '10-31')
 tfreq = 15
 precur_aggr = tfreq
-# experiment = 'fixed_corr'
-experiment = 'adapt_corr'
+experiment = 'fixed_corr'
+# experiment = 'adapt_corr'
 method     = 'leave_2'
 n_boot = 5000
 
