42316c7cb96261f466edf3b8761206d99e988bac
Sem Vijverberg <s.p.vijverberg@vu.nl>
2020-10-15 12:47:10 +0200

    update for cluster

--- publications/paper2/fc_east_US_temp.py
+++ publications/paper2/fc_east_US_temp.py
@@ -110,7 +110,7 @@ precur_aggr = tfreq
 method     = 'leave_2' #ran_strat20
 n_boot = 5000
 
-append_main = ''
+append_main = '_kfold15'
 
 
 
@@ -227,11 +227,11 @@ for month, start_end_TVdate in months.items():
                           append_str=experiment+'_'+month)
         dm[month] = rg.list_for_MI[0].corr_xr.copy()
 
-
+    alphas = np.append(np.logspace(.1, 1.5, num=25), [1E50])
     kwrgs_model = {'scoring':'neg_mean_squared_error',
-                   'alphas':np.append(np.logspace(.1, 1.5, num=25), [1E100]), # large a, strong regul.
-                   'normalize':False}
-                   # 'kfold':15}
+                   'alphas':alphas, # large a, strong regul.
+                   'normalize':False,
+                   'kfold':15}
 
     keys = [k for k in rg.df_data.columns[:-2] if k != rg.TV.name]
     if len(keys) != 0:
@@ -360,7 +360,7 @@ patch2 = mpatches.Patch(color='green', label='Corr. Coef.')
 handles = [patch1, patch2]
 # manually define a new patch
 if len(no_info_fc) != 0:
-    patch = mpatches.Patch(color='red', label='Alpha=1E100')
+    patch = mpatches.Patch(color='red', label='Alpha=1E50')
     handles.append(patch)
 ax.legend(handles=handles,
           fontsize=16, frameon=True, facecolor='grey',
