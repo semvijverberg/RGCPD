826daaa6279b20df50825893ef24cb1fc4d53817
Sem Vijverberg <s.p.vijverberg@vu.nl>
2020-11-16 16:19:13 +0100

    update for cluster

--- publications/paper2/forecasting_temp_and_RW.py
+++ publications/paper2/forecasting_temp_and_RW.py
@@ -66,7 +66,7 @@ remove_PDOyesno = np.array([0, 1])
 seeds = np.array([1,2,3])
 combinations = np.array(np.meshgrid(targets, expers, seeds, remove_PDOyesno)).T.reshape(-1,4)
 
-i_default = 7
+i_default = 0
 
 
 
