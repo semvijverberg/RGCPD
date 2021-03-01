c8cd766d301d858afe9d73b9c610d82aff7a4aca
Sem Vijverberg <s.p.vijverberg@vu.nl>
2021-03-01 11:09:33 +0100

    update for cluster

--- publications/paper_Raed/Causal_SST_forecast.py
+++ publications/paper_Raed/Causal_SST_forecast.py
@@ -64,7 +64,7 @@ from stat_models import plot_importances
 target_datasets = ['USDA_Soy']# , 'USDA_Maize', 'GDHY_Soy']
 seeds = seeds = [1,2,3,4] # ,5]
 yrs = ['1950, 2019'] # ['1950, 2019', '1960, 2019', '1950, 2009']
-methods = ['random_10'] # ['ranstrat_20']
+methods = ['random_20'] # ['ranstrat_20']
 feature_sel = [True]
 combinations = np.array(np.meshgrid(target_datasets,
                                     seeds,
