48281a8a8d3e183967dcf74560d220c389d22627
Sem Vijverberg <s.p.vijverberg@vu.nl>
2022-04-08 18:18:46 +0200

    new structure RGCPD folder

---
+++ RGCPD/df_analysis/df_analysis/filters.py
@@ -0,0 +1,153 @@
+import numpy as np
+import xarray as xr
+import scipy.signal as signal
+
+
+import matplotlib.pyplot as plt
+
+
+def lowpass(ts, period):
+    """ lowpass filter
+    https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.butter.html
+    ts     .. time series array with axis 0 as time axis
+    period .. cutoff period
+    """
+    N  = 2         # Filter order
+    Wn = 1/period  # Cutoff frequency
+    B, A = signal.butter(N, Wn, output='ba')
+    filtered = signal.filtfilt(B, A, ts, axis=(0), padlen=N-1, padtype='constant')
+    
+    if type(ts)==xr.core.dataarray.DataArray:
+        ts_new = ts.copy()
+        ts_new.values = filtered
+    else:
+        ts_new = filtered
+    
+    return ts_new
+
+
+def highpass(ts, period):
+    """ highpass filter
+    https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.butter.html
+    ts     .. time series array with axis 0 as time axis
+    period .. cutoff period
+    """
+    N  = 2         # Filter order
+    Wn = 1/period  # Cutoff frequency
+    B, A = signal.butter(N, Wn, output='ba', btype='highpass')
+    filtered = signal.filtfilt(B, A, ts, axis=(0), padlen=N-1, padtype='constant')
+    
+    if type(ts)==xr.core.dataarray.DataArray:
+        ts_new = ts.copy()
+        ts_new.values = filtered
+    else:
+        ts_new = filtered
+    
+    return ts_new
+
+
+def bandpass(ts, periods):
+    """ highpass filter
+    https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.butter.html
+    ts     .. time series array with axis 0 as time axis
+    period .. cutoff period
+    """
+    N  = 2         # Filter order
+    Wn = 1/periods  # Cutoff frequencies
+    B, A = signal.butter(N, Wn, output='ba', btype='bandpass')
+    filtered = signal.filtfilt(B, A, ts, axis=(0), padlen=N-1, padtype='constant')
+    
+    if type(ts)==xr.core.dataarray.DataArray:
+        ts_new = ts.copy()
+        ts_new.values = filtered
+    else:
+        ts_new = filtered
+    
+    return ts_new
+
+
+def chebychev(ts, period):
+    N, rp = 6, 1  # N=6 was used in Henley et al. (2015), rp is low to minimize ripples
+    Wn = 1/period
+    B, A = signal.cheby1(N, rp, Wn)
+    filtered = signal.filtfilt(B, A, ts, axis=(0), padlen=N-1, padtype='constant')
+    
+    if type(ts)==xr.core.dataarray.DataArray:
+        ts_new = ts.copy()
+        ts_new.values = filtered
+    else:
+        ts_new = filtered
+    
+    return ts_new
+
+
+def notch(ts, period):
+    """ single frequency filter 
+    https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.iirnotch.html
+    period .. 1/f to filter out
+    """
+    w0 = 1/period
+    Q  = 1.#.2# found to work fairly well for deseasonalizing
+    B, A = signal.iirnotch(w0, Q)
+    filtered = signal.filtfilt(B, A, ts, axis=(0), padlen=12)
+    
+    if type(ts)==xr.core.dataarray.DataArray:
+        ts_new = ts.copy()
+        ts_new.values = filtered
+    else:
+        ts_new = filtered
+    
+    return ts_new
+
+
+def deseasonalize_notch(ts):
+    ts_ds = lowpass(lowpass(notch(ts, 12), 12),12)
+    return ts_ds
+
+
+def deseasonalize(ts):
+    """ removing monthly mean climatology """
+    dt_year = (ts.time[12]-ts.time[0]).values  #
+    ts_ds = ts.copy()
+    yrly = ts.groupby_bins('time', np.arange(0,len(ts.time)+1,12)/12*dt_year, right=False).mean(dim='time').rename({'time_bins':'time'})
+    for j in range(12):
+        m = ts.isel(time=slice(j,len(ts)+1,12))
+        ts_ds[j::12,:,:] -= (m-yrly.assign_coords(time=m.time)).mean(dim='time')
+    return ts_ds
+
+
+
+def Lanczos_weights(window, cutoff):
+    """Calculate weights for a low pass Lanczos filter.
+    window: int     The length of the filter window.
+    cutoff: float   The cutoff frequency in inverse time steps.
+    """
+    order = ((window - 1) // 2 ) + 1
+    nwts = 2 * order + 1
+    w = np.zeros([nwts])
+    n = nwts // 2
+    w[n] = 2 * cutoff
+    k = np.arange(1., n)
+    sigma = np.sin(np.pi * k / n) * n / (np.pi * k)
+    firstfactor = np.sin(2. * np.pi * cutoff * k) / (np.pi * k)
+    w[n-1:0:-1] = firstfactor * sigma
+    w[n+1:-1] = firstfactor * sigma
+    weights = xr.DataArray(w[1:-1], dims=['window'])
+    return weights
+
+
+def Lanczos(da, window=121, cutoff=1/120):
+    from xr_regression import xr_lintrend
+    """ Lanczos lowpass filter with reflective boundary """
+    lpw = Lanczos_weights(window=window, cutoff=cutoff)
+    da_trend = xr_lintrend(da)
+    da_dt = da - da_trend
+    da_dt_inv = da_dt.reindex(time=da_dt.time[::-1])
+    extended = xr.concat([da_dt_inv, da_dt, da_dt_inv], 'time')
+    extended.time.values = np.arange(len(extended.time))
+    pos = int(len(extended.time.values)/3)
+    da_filtered = extended.rolling(time=window, center=True).construct('window').dot(lpw)[pos:2*pos]/lpw.sum()
+    da_filtered.time.values = da.time
+    da_filtered = da_filtered + da_trend
+#     print(da_filtered)
+    return da_filtered
---
+++ clustering/enums.py
@@ -0,0 +1,379 @@
+from enum import IntEnum, auto
+
+
+class Country(IntEnum):
+    """Country Codes (ISO 3166-1 alpha-2)"""
+
+    AD = auto()  # Andorra
+    AE = auto()  # United Arab Emirates
+    AF = auto()  # Afghanistan
+    AG = auto()  # Antigua and Barbuda
+    AI = auto()  # Anguilla
+    AL = auto()  # Albania
+    AM = auto()  # Armenia
+    AO = auto()  # Angola
+    AQ = auto()  # Antarctica
+    AR = auto()  # Argentina
+    AS = auto()  # American Samoa
+    AT = auto()  # Austria
+    AU = auto()  # Australia
+    AW = auto()  # Aruba
+    AX = auto()  # Åland Islands
+    AZ = auto()  # Azerbaijan
+    BA = auto()  # Bosnia and Herzegovina
+    BB = auto()  # Barbados
+    BD = auto()  # Bangladesh
+    BE = auto()  # Belgium
+    BF = auto()  # Burkina Faso
+    BG = auto()  # Bulgaria
+    BH = auto()  # Bahrain
+    BI = auto()  # Burundi
+    BJ = auto()  # Benin
+    BL = auto()  # Saint Barthélemy
+    BM = auto()  # Bermuda
+    BN = auto()  # Brunei Darussalam
+    BO = auto()  # Bolivia (Plurinational State of)
+    BQ = auto()  # Bonaire, Sint Eustatius and( Saba
+    BR = auto()  # Brazil
+    BS = auto()  # Bahamas
+    BT = auto()  # Bhutan
+    BV = auto()  # Bouvet Island
+    BW = auto()  # Botswana
+    BY = auto()  # Belarus
+    BZ = auto()  # Belize
+    CA = auto()  # Canada
+    CC = auto()  # Cocos (Keeling) Islands
+    CD = auto()  # Congo, Democratic Republic of the
+    CF = auto()  # Central African Republic
+    CG = auto()  # Congo
+    CH = auto()  # Switzerland
+    CI = auto()  # Côte d'Ivoire
+    CK = auto()  # Cook Islands
+    CL = auto()  # Chile
+    CM = auto()  # Cameroon
+    CN = auto()  # China
+    CO = auto()  # Colombia
+    CR = auto()  # Costa Rica
+    CU = auto()  # Cuba
+    CV = auto()  # Cabo Verde
+    CW = auto()  # Curaçao
+    CX = auto()  # Christmas Island
+    CY = auto()  # Cyprus
+    CZ = auto()  # Czechia
+    DE = auto()  # Germany
+    DJ = auto()  # Djibouti
+    DK = auto()  # Denmark
+    DM = auto()  # Dominica
+    DO = auto()  # Dominican Republic
+    DZ = auto()  # Algeria
+    EC = auto()  # Ecuador
+    EE = auto()  # Estonia
+    EG = auto()  # Egypt
+    EH = auto()  # Western Sahara
+    ER = auto()  # Eritrea
+    ES = auto()  # Spain
+    ET = auto()  # Ethiopia
+    FI = auto()  # Finland
+    FJ = auto()  # Fiji
+    FK = auto()  # Falkland Islands (Malvinas)
+    FM = auto()  # Micronesia (Federated States of)
+    FO = auto()  # Faroe Islands
+    FR = auto()  # France
+    GA = auto()  # Gabon
+    GB = auto()  # United Kingdom of Great Britain and Northern Ireland
+    GD = auto()  # Grenada
+    GE = auto()  # Georgia
+    GF = auto()  # French Guiana
+    GG = auto()  # Guernsey
+    GH = auto()  # Ghana
+    GI = auto()  # Gibraltar
+    GL = auto()  # Greenland
+    GM = auto()  # Gambia
+    GN = auto()  # Guinea
+    GP = auto()  # Guadeloupe
+    GQ = auto()  # Equatorial Guinea
+    GR = auto()  # Greece
+    GS = auto()  # South Georgia and the South Sandwich Islands
+    GT = auto()  # Guatemala
+    GU = auto()  # Guam
+    GW = auto()  # Guinea-Bissau
+    GY = auto()  # Guyana
+    HK = auto()  # Hong Kong
+    HM = auto()  # Heard Island and McDonald Islands
+    HN = auto()  # Honduras
+    HR = auto()  # Croatia
+    HT = auto()  # Haiti
+    HU = auto()  # Hungary
+    ID = auto()  # Indonesia
+    IE = auto()  # Ireland
+    IL = auto()  # Israel
+    IM = auto()  # Isle of Man
+    IN = auto()  # India
+    IO = auto()  # British Indian Ocean Territory
+    IQ = auto()  # Iraq
+    IR = auto()  # Iran (Islamic Republic of)
+    IS = auto()  # Iceland
+    IT = auto()  # Italy
+    JE = auto()  # Jersey
+    JM = auto()  # Jamaica
+    JO = auto()  # Jordan
+    JP = auto()  # Japan
+    KE = auto()  # Kenya
+    KG = auto()  # Kyrgyzstan
+    KH = auto()  # Cambodia
+    KI = auto()  # Kiribati
+    KM = auto()  # Comoros
+    KN = auto()  # Saint Kitts and Nevis
+    KP = auto()  # Korea (Democratic People's Republic of)
+    KR = auto()  # Korea, Republic of
+    KW = auto()  # Kuwait
+    KY = auto()  # Cayman Islands
+    KZ = auto()  # Kazakhstan
+    LA = auto()  # Lao People's Democratic Republic
+    LB = auto()  # Lebanon
+    LC = auto()  # Saint Lucia
+    LI = auto()  # Liechtenstein
+    LK = auto()  # Sri Lanka
+    LR = auto()  # Liberia
+    LS = auto()  # Lesotho
+    LT = auto()  # Lithuania
+    LU = auto()  # Luxembourg
+    LV = auto()  # Latvia
+    LY = auto()  # Libya
+    MA = auto()  # Morocco
+    MC = auto()  # Monaco
+    MD = auto()  # Moldova, Republic of
+    ME = auto()  # Montenegro
+    MF = auto()  # Saint Martin (French part)
+    MG = auto()  # Madagascar
+    MH = auto()  # Marshall Islands
+    MK = auto()  # Macedonia, the former Yugoslav Republic of
+    ML = auto()  # Mali
+    MM = auto()  # Myanmar
+    MN = auto()  # Mongolia
+    MO = auto()  # Macao
+    MP = auto()  # Northern Mariana Islands
+    MQ = auto()  # Martinique
+    MR = auto()  # Mauritania
+    MS = auto()  # Montserrat
+    MT = auto()  # Malta
+    MU = auto()  # Mauritius
+    MV = auto()  # Maldives
+    MW = auto()  # Malawi
+    MX = auto()  # Mexico
+    MY = auto()  # Malaysia
+    MZ = auto()  # Mozambique
+    NA = auto()  # Namibia
+    NC = auto()  # New Caledonia
+    NE = auto()  # Niger
+    NF = auto()  # Norfolk Island
+    NG = auto()  # Nigeria
+    NI = auto()  # Nicaragua
+    NL = auto()  # Netherlands
+    NO = auto()  # Norway
+    NP = auto()  # Nepal
+    NR = auto()  # Nauru
+    NU = auto()  # Niue
+    NZ = auto()  # New Zealand
+    OM = auto()  # Oman
+    PA = auto()  # Panama
+    PE = auto()  # Peru
+    PF = auto()  # French Polynesia
+    PG = auto()  # Papua New Guinea
+    PH = auto()  # Philippines
+    PK = auto()  # Pakistan
+    PL = auto()  # Poland
+    PM = auto()  # Saint Pierre and Miquelon
+    PN = auto()  # Pitcairn
+    PR = auto()  # Puerto Rico
+    PS = auto()  # Palestine, State of
+    PT = auto()  # Portugal
+    PW = auto()  # Palau
+    PY = auto()  # Paraguay
+    QA = auto()  # Qatar
+    RE = auto()  # Réunion
+    RO = auto()  # Romania
+    RS = auto()  # Serbia
+    RU = auto()  # Russian Federation
+    RW = auto()  # Rwanda
+    SA = auto()  # Saudi Arabia
+    SB = auto()  # Solomon Islands
+    SC = auto()  # Seychelles
+    SD = auto()  # Sudan
+    SE = auto()  # Sweden
+    SG = auto()  # Singapore
+    SH = auto()  # Saint Helena, Ascension and Tristan da Cunha
+    SI = auto()  # Slovenia
+    SJ = auto()  # Svalbard and Jan Mayen
+    SK = auto()  # Slovakia
+    SL = auto()  # Sierra Leone
+    SM = auto()  # San Marino
+    SN = auto()  # Senegal
+    SO = auto()  # Somalia
+    SR = auto()  # Suriname
+    SS = auto()  # South Sudan
+    ST = auto()  # Sao Tome and Principe
+    SV = auto()  # El Salvador
+    SX = auto()  # Sint Maarten (Dutch part)
+    SY = auto()  # Syrian Arab Republic
+    SZ = auto()  # Eswatini
+    TC = auto()  # Turks and Caicos Islands
+    TD = auto()  # Chad
+    TF = auto()  # French Southern Territories
+    TG = auto()  # Togo
+    TH = auto()  # Thailand
+    TJ = auto()  # Tajikistan
+    TK = auto()  # Tokelau
+    TL = auto()  # Timor-Leste
+    TM = auto()  # Turkmenistan
+    TN = auto()  # Tunisia
+    TO = auto()  # Tonga
+    TR = auto()  # Turkey
+    TT = auto()  # Trinidad and Tobago
+    TV = auto()  # Tuvalu
+    TW = auto()  # Taiwan, Province of China
+    TZ = auto()  # Tanzania, United Republic of
+    UA = auto()  # Ukraine
+    UG = auto()  # Uganda
+    UM = auto()  # United States Minor Outlying Islands
+    US = auto()  # United States of America
+    UY = auto()  # Uruguay
+    UZ = auto()  # Uzbekistan
+    VA = auto()  # Holy See
+    VC = auto()  # Saint Vincent and the Grenadines
+    VE = auto()  # Venezuela (Bolivarian Republic of)
+    VG = auto()  # Virgin Islands (British)
+    VI = auto()  # Virgin Islands
+    VN = auto()  # Viet Nam
+    VU = auto()  # Vanuatu
+    WF = auto()  # Wallis and Futuna
+    WS = auto()  # Samoa
+    YE = auto()  # Yemen
+    YT = auto()  # Mayotte
+    ZA = auto()  # South Africa
+    ZM = auto()  # Zambia
+    ZW = auto()  # Zimbabwe
+
+class Continents(IntEnum):
+    Africa          = auto()
+    Asia            = auto()
+    South_America   = auto()
+    Europe          = auto()
+    Oceania         = auto()
+    North_America   = auto()
+
+class US_States(IntEnum):
+    AL = auto() # Alabama
+    AK = auto() # Alaska
+    AS = auto() # American Samoa
+    AZ = auto() # Arizona
+    AR = auto() # Arkansas
+    CA = auto() # California
+    CO = auto() # Colorado
+    CT = auto() # Connecticut
+    DE = auto() # Delaware
+    DC = auto() # District of Columbia
+    FL = auto() # Florida
+    GA = auto() # Georgia
+    GU = auto() # Guam
+    HI = auto() # Hawaii
+    ID = auto() # Idaho
+    IL = auto() # Illinois
+    IN = auto() # Indiana
+    IA = auto() # Iowa
+    KS = auto() # Kansas
+    KY = auto() # Kentucky
+    LA = auto() # Louisiana
+    ME = auto() # Maine
+    MD = auto() # Maryland
+    MA = auto() # Massachusetts
+    MI = auto() # Michigan
+    MN = auto() # Minnesota
+    MS = auto() # Mississippi
+    MO = auto() # Missouri
+    MT = auto() # Montana
+    NE = auto() # Nebraska
+    NV = auto() # Nevada
+    NH = auto() # New Hampshire
+    NJ = auto() # New Jersey
+    NM = auto() # New Mexico
+    NY = auto() # New York
+    NC = auto() # North Carolina
+    ND = auto() # North Dakota
+    MP = auto() # Northern Mariana Islands
+    OH = auto() # Ohio
+    OK = auto() # Oklahoma
+    OR = auto() # Oregon
+    PA = auto() # Pennsylvania
+    PR = auto() # Puerto Rico
+    RI = auto() # Rhode Island
+    SC = auto() # South Carolina
+    SD = auto() # South Dakota
+    TN = auto() # Tennessee
+    TX = auto() # Texas
+    UT = auto() # Utah
+    VT = auto() # Vermont
+    VI = auto() # Virgin Islands
+    VA = auto() # Virginia
+    WA = auto() # Washington
+    WV = auto() # West Virginia
+    WI = auto() # Wisconsin
+    WY = auto() # Wyoming
+us_state_abbrev = {
+                    'Alabama': 'AL',
+                    'Alaska': 'AK',
+                    'American Samoa': 'AS',
+                    'Arizona': 'AZ',
+                    'Arkansas': 'AR',
+                    'California': 'CA',
+                    'Colorado': 'CO',
+                    'Connecticut': 'CT',
+                    'Delaware': 'DE',
+                    'District of Columbia': 'DC',
+                    'Florida': 'FL',
+                    'Georgia': 'GA',
+                    'Guam': 'GU',
+                    'Hawaii': 'HI',
+                    'Idaho': 'ID',
+                    'Illinois': 'IL',
+                    'Indiana': 'IN',
+                    'Iowa': 'IA',
+                    'Kansas': 'KS',
+                    'Kentucky': 'KY',
+                    'Louisiana': 'LA',
+                    'Maine': 'ME',
+                    'Maryland': 'MD',
+                    'Massachusetts': 'MA',
+                    'Michigan': 'MI',
+                    'Minnesota': 'MN',
+                    'Mississippi': 'MS',
+                    'Missouri': 'MO',
+                    'Montana': 'MT',
+                    'Nebraska': 'NE',
+                    'Nevada': 'NV',
+                    'New Hampshire': 'NH',
+                    'New Jersey': 'NJ',
+                    'New Mexico': 'NM',
+                    'New York': 'NY',
+                    'North Carolina': 'NC',
+                    'North Dakota': 'ND',
+                    'Northern Mariana Islands':'MP',
+                    'Ohio': 'OH',
+                    'Oklahoma': 'OK',
+                    'Oregon': 'OR',
+                    'Pennsylvania': 'PA',
+                    'Puerto Rico': 'PR',
+                    'Rhode Island': 'RI',
+                    'South Carolina': 'SC',
+                    'South Dakota': 'SD',
+                    'Tennessee': 'TN',
+                    'Texas': 'TX',
+                    'Utah': 'UT',
+                    'Vermont': 'VT',
+                    'Virgin Islands': 'VI',
+                    'Virginia': 'VA',
+                    'Washington': 'WA',
+                    'West Virginia': 'WV',
+                    'Wisconsin': 'WI',
+                    'Wyoming': 'WY'
+                    }
