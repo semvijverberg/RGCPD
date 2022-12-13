af663f3c8429c11db3500d74a0541b2356e8637e
Sem Vijverberg <s.p.vijverberg@vu.nl>
2022-12-13 13:54:08 +0100

    adding plot with difference OOS target consequences for skill

---
+++ publications/Vijverberg_et_al_2022_AIES/OOS_pp_consequence.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 13 10:52:40 2022
+
+@author: semvijverberg
+"""
+import os, inspect, sys
+if '/Users/semvijverberg/surfdrive/Scripts/jannes_code/RGCPD' in sys.path:
+    sys.path.remove('/Users/semvijverberg/surfdrive/Scripts/jannes_code/RGCPD')
+import matplotlib as mpl
+if sys.platform == 'linux':
+    mpl.use('Agg')
+    n_cpu = 8
+else:
+    n_cpu = 3
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+# import argparse
+# from matplotlib.lines import Line2D
+# import csv
+# import re
+
+user_dir = os.path.expanduser('~')
+os.chdir(os.path.join(user_dir,
+                      'surfdrive/Scripts/RGCPD/publications/Vijverberg_et_al_2022_AIES/'))
+curr_dir = os.path.join(user_dir, 'surfdrive/Scripts/RGCPD/RGCPD/')
+main_dir = '/'.join(curr_dir.split('/')[:-2])
+if main_dir not in sys.path:
+    sys.path.append(main_dir)
+
+import utils_paper3
+
+path_main = '/Users/semvijverberg/Desktop/cluster/surfdrive/output_paper3'
+standard_subfolder = 'a9943/USDA_Soy_clusters__1/timeseriessplit_25/s1'
+model_name = 'LogisticRegression'
+fc_month = 'March'
+experiment = 'test_no_areaw'
+
+
+load = 'all'
+load_models = False
+save = True
+fc_types = [0.31, 0.33, 0.35]
+fc_type = fc_types[1]
+btoos = '_T' # if btoos=='_T': binary target out of sample.
+# btoos = '_theor' # binary target based on gaussian quantile
+# btoos = False, Events are based on whole dataset (even though
+# detrending and standardizing are done out of sample)
+
+path_1_no_OOS = f'{experiment}/{standard_subfolder}/df_data_{fc_type}{btoos}_all_CD'
+
+
+def plot_table(subtitle, path, ax):
+
+    target_options = [['Target', 'Target | PPS']]
+    out = utils_paper3.load_scores(target_options[0], model_name, model_name,  2000,
+                                   path, condition=None)
+    df_scores, df_boots, df_preds = out
+
+    # correct order ['All, '50%', '30%']
+    df_scores = [df_scores[i] for i in [0,2,1]]
+    df_test_m = [d[fc_month] for d in df_scores]
+    table_col = [d.index[0] for d in df_test_m][1:]
+    order_verif = ['All'] + ['Top '+t.split(' ')[1] for t in table_col]
+    df_scores = []
+    for i, df_test_skill in enumerate(df_test_m):
+        r = {df_test_skill.index[0]:order_verif[i]}
+        df_scores.append( df_test_skill.rename(r, axis=0) )
+    df_scores = pd.concat(df_scores, axis=1)
+    df_scores = df_scores.T.reset_index().pivot_table(index='index')
+    df_scores = df_scores[order_verif]
+
+    rename_met = {'RMSE':'RMSE-SS', 'corrcoef':'Corr.', 'MAE':'MAE-SS',
+                  'BSS':'BSS', 'roc_auc_score':'AUC', 'r2_score':'$r^2$',
+                  'mean_absolute_percentage_error':'MAPE', 'AUC_SS':'AUC-SS',
+                  'precision':'Precision', 'accuracy':'Accuracy'}
+    metrics_plot = ['BSS', 'accuracy', 'precision']
+
+
+    tsc = df_scores.loc[metrics_plot].values
+    tscs = np.zeros(tsc.shape, dtype=object)
+    for i, m in enumerate(metrics_plot):
+        _sc_ = tsc[i]
+        if 'prec' in m or 'acc' in m:
+            sc_f = ['{:.0f}%'.format(round(s,0)) for s in _sc_.ravel()]
+            # if df_boots_list is not None:
+            #     for j, s in enumerate(zip(tbl[i], tbh[i])):
+            #         sc_f[j] = sc_f[j].replace('%', r'$_{{{:.0f}}}^{{{:.0f}}}$%'.format(*s))
+        else:
+            sc_f = ['{:.2f}'.format(round(s,2)) for s in tsc[i].ravel()]
+
+            # if df_boots_list is not None:
+            #     for j, s in enumerate(zip(tbl[i], tbh[i])):
+            #         sc_f[j] = sc_f[j] + r'$_{{{:.2f}}}^{{{:.2f}}}$'.format(*s)
+        tscs[i] = sc_f
+
+
+    table = ax.table(cellText=tscs,
+                      cellLoc='center',
+                      rowLabels=[rename_met[m] for m in metrics_plot],
+                      colLabels=df_scores.columns, loc='center',
+                      edges='closed')
+    table.set_fontsize(20)
+    if len(df_test_m) == 2:
+        table.scale(0.9, 1.7)
+    elif len(df_test_m) == 3:
+        table.scale(1, 3)
+    ax.axis('off')
+    ax.axis('tight')
+    ax.set_facecolor('white')
+    ax.set_title(subtitle, fontsize=20)
+    return ax
+
+fig, ax = plt.subplots(3, 1, figsize=(7,10))
+# plt.subplots_adjust(hspace=5)
+ax1 = plot_table('No OOS pre-processing', os.path.join(path_main, path_1_no_OOS), ax[0])
+ax2 = plot_table('No OOS pre-processing', os.path.join(path_main, path_1_no_OOS), ax[1])
+ax3 = plot_table('No OOS pre-processing', os.path.join(path_main, path_1_no_OOS), ax[2])
