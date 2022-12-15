2c4a761d9d91779b4e925e30414b95ccb335e0c4
Sem Vijverberg <s.p.vijverberg@vu.nl>
2022-12-15 11:11:06 +0100

    update for cluster

--- publications/Vijverberg_et_al_2022_AIES/OOS_pp_consequence.py
+++ publications/Vijverberg_et_al_2022_AIES/OOS_pp_consequence.py
@@ -34,50 +34,47 @@ if main_dir not in sys.path:
 import utils_paper3
 
 path_main = '/Users/semvijverberg/Desktop/cluster/surfdrive/output_paper3'
-standard_subfolder = 'a9943/USDA_Soy_clusters__1/timeseriessplit_25/s1'
+standard_subfolder = 'a9943/USDA_Soy_clusters__1/timeseriessplit_30/s1'
 model_name = 'LogisticRegression'
-fc_month = 'March'
-experiment = 'test_no_areaw'
+fc_month = 'April'
 
 
-load = 'all'
-load_models = False
-save = True
 fc_types = [0.31, 0.33, 0.35]
-fc_type = fc_types[1]
-btoos = '_T' # if btoos=='_T': binary target out of sample.
-# btoos = '_theor' # binary target based on gaussian quantile
-# btoos = False, Events are based on whole dataset (even though
-# detrending and standardizing are done out of sample)
+fc_type = fc_types[0]
 
-path_1_no_OOS = f'{experiment}/{standard_subfolder}/df_data_{fc_type}{btoos}_all_CD'
+path_no_OOS = f'test_no_areaw_oosT_False/{standard_subfolder}/df_data_fctpFalse_all'
+path_OOS_no_q = f'test_no_areaw_oosT_True/{standard_subfolder}/df_data_fctpFalse_all'
+path_OOS = f'test_no_areaw_oosT_True/{standard_subfolder}/df_data_fctp_T_all'
 
 
 def plot_table(subtitle, path, ax):
 
-    target_options = [['Target', 'Target | PPS']]
-    out = utils_paper3.load_scores(target_options[0], model_name, model_name,  2000,
-                                   path, condition=None)
-    df_scores, df_boots, df_preds = out
-
-    # correct order ['All, '50%', '30%']
-    df_scores = [df_scores[i] for i in [0,2,1]]
-    df_test_m = [d[fc_month] for d in df_scores]
-    table_col = [d.index[0] for d in df_test_m][1:]
-    order_verif = ['All'] + ['Top '+t.split(' ')[1] for t in table_col]
     df_scores = []
-    for i, df_test_skill in enumerate(df_test_m):
-        r = {df_test_skill.index[0]:order_verif[i]}
-        df_scores.append( df_test_skill.rename(r, axis=0) )
+    for fc_type in fc_types:
+        orig_fc_type = path.split('df_data_')[1][:4]
+        path = path.replace(orig_fc_type, str(fc_type))
+        target_options = [['Target', 'Target | PPS']]
+        out = utils_paper3.load_scores(target_options[0], model_name, model_name, 2000,
+                                       path, condition=['strong 30%'])
+        df_scores_, df_boots, df_preds = out
+
+        # correct order ['All, '50%', '30%']
+        df_scores_ = [df_scores_[i] for i in [0,1]]
+        df_test_m = [d[fc_month] for d in df_scores_]
+        table_col = [d.index[0] for d in df_test_m][1:]
+        order_verif = [f'All (q={fc_type})'] + ['Top '+t.split(' ')[1] + f' (q={fc_type})' for t in table_col]
+        for i, df_test_skill in enumerate(df_test_m):
+            r = {df_test_skill.index[0]:order_verif[i]}
+            df_scores.append( df_test_skill.rename(r, axis=0) )
     df_scores = pd.concat(df_scores, axis=1)
     df_scores = df_scores.T.reset_index().pivot_table(index='index')
-    df_scores = df_scores[order_verif]
+    # df_scores = df_scores[order_verif]
 
     rename_met = {'RMSE':'RMSE-SS', 'corrcoef':'Corr.', 'MAE':'MAE-SS',
                   'BSS':'BSS', 'roc_auc_score':'AUC', 'r2_score':'$r^2$',
                   'mean_absolute_percentage_error':'MAPE', 'AUC_SS':'AUC-SS',
                   'precision':'Precision', 'accuracy':'Accuracy'}
-    metrics_plot = ['BSS', 'accuracy', 'precision']
+    metrics_plot = ['BSS', 'precision']
 
 
     tsc = df_scores.loc[metrics_plot].values
@@ -103,19 +100,18 @@ def plot_table(subtitle, path, ax):
                       rowLabels=[rename_met[m] for m in metrics_plot],
                       colLabels=df_scores.columns, loc='center',
                       edges='closed')
-    table.set_fontsize(20)
-    if len(df_test_m) == 2:
-        table.scale(0.9, 1.7)
-    elif len(df_test_m) == 3:
-        table.scale(1, 3)
+    table.set_fontsize(30)
+
+    table.scale(1, 3)
     ax.axis('off')
     ax.axis('tight')
     ax.set_facecolor('white')
-    ax.set_title(subtitle, fontsize=20)
+    ax.set_title(subtitle, fontsize=16)
     return ax
 
-fig, ax = plt.subplots(3, 1, figsize=(7,10))
+fig, ax = plt.subplots(3, 1, figsize=(10,7.2))
 # plt.subplots_adjust(hspace=5)
-ax1 = plot_table('No OOS pre-processing', os.path.join(path_main, path_1_no_OOS), ax[0])
-ax2 = plot_table('No OOS pre-processing', os.path.join(path_main, path_1_no_OOS), ax[1])
-ax3 = plot_table('No OOS pre-processing', os.path.join(path_main, path_1_no_OOS), ax[2])
+ax1 = plot_table('No OOS pre-processing', os.path.join(path_main, path_no_OOS), ax[0])
+ax2 = plot_table('OOS pre-processing, in-sample quantiles', os.path.join(path_main, path_OOS_no_q), ax[1])
+ax3 = plot_table('OOS pre-processing', os.path.join(path_main, path_OOS), ax[2])
+fig.suptitle(f'{fc_month}, quantile: {fc_type}', y=0.96, fontsize=22)
